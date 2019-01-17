185f16a313dfcf5916a92fe3f9db237ad605af2a
liuhuihui <244701374@qq.com>
2019-01-17 11:12:06 +0800

    音乐页:播放

--- src/components/my/playMusic.js
+++ src/components/my/playMusic.js
@@ -1,5 +1,5 @@
 import React,{Component} from 'react';
-import { NavBar, Icon } from 'antd-mobile';
+import { NavBar, Icon, Slider } from 'antd-mobile';
 import admin from '../../assets/images/admin.png';
 
 /**
@@ -11,11 +11,12 @@ class PlayMusic extends Component{
   constructor(props) {
       super(props);
       this.state={
-
+        percent: 50,
       }
   }
 
   render (){
+    const { percent } = this.state;
       return(
           <div className='m-my'>
               <div className="m-my-play">
@@ -34,13 +35,18 @@ class PlayMusic extends Component{
 
                   {/*animate*/}
                   <div className="m-my-play-con">
-                    {/*animated infinite rotateOut*/}
                     <div className="m-my-play-con-w">
-                      {/*animated infinite rotateOut delay-2s*/}
-                      <div className="">
+                      <div className="m-my-play-con-w-b">
+                        <div className="m-my-play-con-w-q"></div>
                       </div>
+                      <div className="m-my-play-con-w-q"></div>
                     </div>
-                    {/*animated infinite heartBeat*/}
+                    <div className="m-my-play-con-w m-my-play-con-w2">
+                      <div className="m-my-play-con-w-b">
+                        <div className="m-my-play-con-w-q"></div>
+                      </div>
+                      <div className="m-my-play-con-w-q"></div>
+                    </div>
                     <div className="m-my-play-con-n">
                       <img src={admin} alt=""/>
                     </div>
@@ -48,9 +54,49 @@ class PlayMusic extends Component{
 
                   {/*bot*/}
                   <div className="m-my-play-bot">
-
+                      <div className="m-my-play-bot-t">
+                        <span><i className="icon-bf-live" /></span>
+                        <span><i className="icon-bf-xz" /></span>
+                        <span><i className="icon-bf-xx" /></span>
+                        <span><i className="icon-bf-more" /></span>
+                      </div>
+                      <div className="m-my-play-bot-c">
+                          <span>00:02</span>
+                          <Slider
+                            defaultValue={3}
+                            min={0}
+                            max={10}
+                            step={0.1}
+                            onChange={()=>console.log('change')}
+                            onAfterChange={()=>console.log('afterChange')}
+                          />
+                          <span>04:34</span>
+                      </div>
+                      <div className="m-my-play-bot-b">
+                        <span><i className="icon-bf-xh" /></span>
+                        <span><i className="icon-bf-l" /></span>
+                        <span><i className="icon-bf-bf" style={{fontSize:38}}/></span>
+                        <span><i className="icon-bf-r" /></span>
+                        <span><i className="icon-bf-list" /></span>
+                      </div>
                   </div>
 
+                  {/*list*/}
+                  <div className="m-my-play-list">
+                    <div>
+                      <div className="m-my-play-list-t">
+                        <span><i className="icon-bf-list-xh" />列表循环</span>
+                        <span><i className="icon-bf-list-sc" />收藏</span>
+                        <span><i className="icon-bf-list-del" /></span>
+                      </div>
+                      <div className="m-my-play-list-b">
+
+                      </div>
+                      <div className="m-my-play-list-close">
+                        <span>关闭</span>
+                      </div>
+                    </div>
+                  </div>
               </div>
           </div>
       )
