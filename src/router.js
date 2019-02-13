f174afb851eba82860b3414352fccc3b418c20ca
liuhuihui <244701374@qq.com>
2019-02-13 14:41:56 +0800

    添加路由

--- src/router.js
+++ src/router.js
@@ -4,6 +4,7 @@ import Footer from './containers/footer';                  //我的
 import Lists from './components/my/myLists';               //我的 - 列表
 import PlayMusic from './components/my/playMusic';         //我的 - 播放音乐
 import RecordLists from './components/my/recordLists'      //我的 - 最近播放列表
+import CollectLists from './components/my/collectLists'    //我的 - 收藏列表
 
 function RouterConfig({ history }) {
   return (
@@ -12,6 +13,7 @@ function RouterConfig({ history }) {
         {/*<Route path="/" exact component={IndexPage} />*/}
         <Route path="/myMusic" component={Footer} />
         <Route path="/recordLists" component={RecordLists} />
+        <Route path="/collectLists" component={CollectLists}/>
         <Route path="/lists:id" component={Lists} />
         <Route path="/playMusic" component={PlayMusic} />
       </Switch>
