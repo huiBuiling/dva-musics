c08b2c685623e76682890d47ae04c93176c1695a
liuhuihui <244701374@qq.com>
2019-03-05 11:59:05 +0800

    调整

--- src/components/my/playMusicLists.js
+++ src/components/my/playMusicLists.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
-import { connect } from 'dva';
-import { withRouter } from 'dva/router';
+import {connect} from 'dva';
+import {withRouter} from 'dva/router';
 import cricle from '../../assets/images/cricle.png';
 import cricleUn from '../../assets/images/uncricle.png';
 
@@ -19,9 +19,9 @@ class PlayMusicLists extends Component {
     }
 
     //移除某一首歌曲
-    delList = (id)=>{
+    delList = (id) => {
         //判断是否全部清空或最后一个
-        if(this.props.playMusicList.length === 1){
+        if (this.props.playMusicList.length === 1) {
             const audio = document.getElementById('audio');
             audio.pause();
             audio.src = null;
@@ -29,14 +29,14 @@ class PlayMusicLists extends Component {
         }
 
         this.props.dispatch({
-            type:'playMusic/delsPlayMusicList',
-            id:id,
-            flag:false
+            type: 'playMusic/delsPlayMusicList',
+            id: id,
+            flag: false
         })
     }
 
     //清空全部
-    delListAll = ()=>{
+    delListAll = () => {
         const audio = document.getElementById('audio');
         audio.pause();
         audio.src = null;
@@ -44,18 +44,18 @@ class PlayMusicLists extends Component {
 
         //清空数据
         this.props.dispatch({
-            type:'playMusic/delsPlayMusicList',
-            id:null,
-            flag:true
+            type: 'playMusic/delsPlayMusicList',
+            id: null,
+            flag: true
         })
     }
 
     render() {
-        const { current } = this.props;
-        const dataList  = this.props.playMusicList;
+        const {current, playMusicCurrent} = this.props;
+        const dataList = this.props.playMusicList;
         let currentMusic = 0;
-        dataList.filter((item,index) => {
-            if(item.id === current.id){
+        dataList.filter((item, index) => {
+            if (item.id === current.id) {
                 currentMusic = index;
             }
         });
@@ -65,9 +65,9 @@ class PlayMusicLists extends Component {
                     <div className="m-my-play-list-t-t">
                         <span className="fl"><i className="icon-bf-list-xh"/>列表循环</span>
                         <span className="fr">
-                    <span><i className="icon-bf-list-sc"/>收藏</span>
-                    <span onClick={this.delListAll}><i className="icon-bf-list-del"/></span>
-                  </span>
+                            <span><i className="icon-bf-list-sc"/>收藏</span>
+                            <span onClick={this.delListAll}><i className="icon-bf-list-del"/></span>
+                        </span>
                     </div>
                     <div className="m-my-play-list-t-b">
                         <ul>
@@ -80,10 +80,13 @@ class PlayMusicLists extends Component {
                                         this.setState({isActive: index});
                                     }}>
                                         <span><img src={currentMusic === index ? cricle : cricleUn} alt=""/>{item.name}</span>
-                                        <span className="singer"> - </span>
-                                        {/*<span className="singer">{item.ar[0].name}</span>*/}
+                                        {/*{playMusicCurrent.station === null || playMusicCurrent.station === undefined &&
+                                           <span className="singer"> - {item.ar[0].name}</span>
+                                        }*/}
                                     </div>
-                                    <span onClick={() => {this.delList(item.id)}}>
+                                    <span onClick={() => {
+                                        this.delList(item.id)
+                                    }}>
                                         <i className="icon-close2"/>
                                     </span>
                                 </li>
@@ -100,9 +103,10 @@ class PlayMusicLists extends Component {
     }
 }
 
-const mapStateToProps = (state,dispatch)=>{
-    return{
-        playMusicList:state.playMusic.playMusicList
+const mapStateToProps = (state, dispatch) => {
+    return {
+        playMusicList: state.playMusic.playMusicList,
+        playMusicCurrent: state.playMusic.playMusicCurrent
     }
 }
 export default connect(mapStateToProps)(PlayMusicLists);
